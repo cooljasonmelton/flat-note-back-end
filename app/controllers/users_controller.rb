fc0a5b22afb3cdbdce13f7b3407f18a516630cbb
cooljasonmelton <cooljasonmelton>
2020-05-05 15:27:19 -0500

    find or create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < ApplicationController
-    
+    def index
+        users = User.all
+        render json: users, include: [:notes, :tags]
+    end
+
+    def create
+        user = User.find_or_create_by(name: params[:user])
+        render json: user, include: [:notes, :tags]
+    end
 end
