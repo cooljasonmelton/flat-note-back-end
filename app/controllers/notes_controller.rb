a522f822f578f9bb749e44745df4a0004dba60a0
cooljasonmelton <cooljasonmelton>
2020-05-06 21:24:37 -0500

    create notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,17 @@
 class NotesController < ApplicationController
+
+    def create
+        puts params
+        new_note = Note.create(name: params[:name], text: params[:text], user_id: params[:userId])
+        
+        params[:tags].split(', ').each do |tag|
+            new_note.tags << Tag.create(name: tag)
+        end 
+       
+        user = User.find_by(id: params[:userId])
+        render json: user, include: [:notes, :tags]
+    end
 end
+
+
+# {"userId"=>4, "name"=>"a", "text"=>"c", "tags"=>"s", "controller"=>"notes", "action"=>"create", "note"=>{"name"=>"a", "text"=>"c"}}
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
-    def index
-        users = User.all
-        render json: users, include: [:notes, :tags]
-    end
+
 
     def create
         user = User.find_or_create_by(name: params[:user])
